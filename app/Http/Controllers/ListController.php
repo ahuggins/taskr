8b39f4dd952278a6e1d57ee25b86c3248d93287e
Andrew huggins <andrewhuggins@gmail.com>
2015-08-04 01:41:16 -0400

    add middleware to listController

--- app/Http/Controllers/ListController.php
+++ app/Http/Controllers/ListController.php
@@ -11,6 +11,10 @@ use Auth;
 
 class ListController extends Controller
 {
+
+    public function __construct() {
+        $this->middleware('auth');
+    }
     /**
      * Display a listing of the resource.
      *
